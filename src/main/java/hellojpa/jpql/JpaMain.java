9526e336641f367f5682b1b71e08eb802139827b
Ji, jeonguk <dkdpawjddnr@naver.com>
2025-07-11 23:13:35 +0900

    페이징 API

--- src/main/java/hellojpa/jpql/JpaMain.java
+++ src/main/java/hellojpa/jpql/JpaMain.java
@@ -14,34 +14,25 @@ public class JpaMain {
         tx.begin();
 
         try {
-            Member member = new Member();
-            member.setUsername("member1");
-            member.setAge(10);
-            em.persist(member);
+            for(int i = 0; i < 100; i++){
+                Member member = new Member();
+                member.setUsername("member" + i);
+                member.setAge(i);
+                em.persist(member);
+            }
 
-            // 엔티티타입 프로젝션
-            List<Member> result = em.createQuery("select m from Member m", Member.class)
+            em.flush();
+            em.clear();
+
+            List<Member> result = em.createQuery("select m from Member m order by m.age desc", Member.class)
+                    .setFirstResult(1)
+                    .setMaxResults(10)
                     .getResultList();
 
-            // 임베디드타입 프로젝션
-            List<Address> resultList = em.createQuery("select o.address from Order o", Address.class)
-                    .getResultList();
-
-            // 스칼라타입 프로젝션
-            List<Object[]> objects = em.createQuery("select m.username, m.age from Member m")
-                    .getResultList();
-
-            // 스칼라타입 프로젝션
-            List<MemberDTO> memberDtos = em.createQuery("select new hellojpa.jpql.MemberDTO(m.username, m.age) from Member m", MemberDTO.class)
-                    .getResultList();
-
-            Object[] results = objects.get(0);
-            System.out.println("username = " + results[0]);
-            System.out.println("age = " + results[1]);
-
-            MemberDTO memberDTO = memberDtos.get(0);
-            System.out.println("memberDTO = " + memberDTO.getUsername());
-            System.out.println("memberDTO = " + memberDTO.getAge());
+            System.out.println("result.size = " + result.size());
+            for (Member member1 : result){
+                System.out.println("member1 = " + member1);
+            }
 
             tx.commit();
         } catch (Exception e){
