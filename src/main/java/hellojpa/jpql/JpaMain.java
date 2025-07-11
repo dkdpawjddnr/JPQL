daefa58e7beb4d4826f898fdb9ae0fd3fd0d0b14
Ji, jeonguk <dkdpawjddnr@naver.com>
2025-07-11 19:34:11 +0900

    엔티티, 임베디드타입, 스칼라타입 프로젝션

--- src/main/java/hellojpa/jpql/JpaMain.java
+++ src/main/java/hellojpa/jpql/JpaMain.java
@@ -2,6 +2,8 @@ package hellojpa.jpql;
 
 import jakarta.persistence.*;
 
+import java.util.List;
+
 public class JpaMain {
 
     public static void main(String[] args) {
@@ -17,11 +19,30 @@ public class JpaMain {
             member.setAge(10);
             em.persist(member);
 
-            Member result = em.createQuery("select m from Member m where m.username = :username", Member.class)
-                            .setParameter("username", "member1")
-                            .getSingleResult();
+            // 엔티티타입 프로젝션
+            List<Member> result = em.createQuery("select m from Member m", Member.class)
+                    .getResultList();
 
-            System.out.println("result = " + result.getUsername());
+            // 임베디드타입 프로젝션
+            List<Address> resultList = em.createQuery("select o.address from Order o", Address.class)
+                    .getResultList();
+
+            // 스칼라타입 프로젝션
+            List<Object[]> objects = em.createQuery("select m.username, m.age from Member m")
+                    .getResultList();
+
+            // 스칼라타입 프로젝션
+            List<MemberDTO> memberDtos = em.createQuery("select new hellojpa.jpql.MemberDTO(m.username, m.age) from Member m", MemberDTO.class)
+                    .getResultList();
+
+            Object[] results = objects.get(0);
+            System.out.println("username = " + results[0]);
+            System.out.println("age = " + results[1]);
+
+            MemberDTO memberDTO = memberDtos.get(0);
+            System.out.println("memberDTO = " + memberDTO.getUsername());
+            System.out.println("memberDTO = " + memberDTO.getAge());
+
             tx.commit();
         } catch (Exception e){
             tx.rollback();
